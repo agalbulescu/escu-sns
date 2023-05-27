be14ed0f979b36883083559ddc8eeb554b3c02a8
Andrei Albulescu <agalbulescu@gmail.com>
2023-05-27 14:27:06 +0300

    Initial commit

---
+++ escu-sns/pages/api/snsData/index.js
@@ -0,0 +1,51 @@
+import UserAgent from 'user-agents';
+const { chromium } = require('playwright');
+
+const userAgent = new UserAgent();
+
+const handler = async (req, res) => {
+  const { address } = req.query;
+
+  const options = {
+    userAgent: userAgent.toString()
+  }
+
+  try {
+
+    console.log("STARTING SOLANA NAME SERVICE WORKER");
+
+    const browser = await chromium.launch({
+        headless: true,
+    })
+
+    const page = await browser.newPage(options)
+    page.setDefaultTimeout(60000);
+
+    await page.setViewportSize({ width: 1200, height: 800 })
+
+    // page.on('request', (request) => console.log('>>', request.method(), request.url()))
+
+    // page.on('response', async (response) => { console.log('<<', response.status(), response.url())})
+
+    const responsePromise = page.waitForResponse(response => response.url() === `https://api.solscan.io/domain?address=${address}&cluster=` && response.status() === 200);
+    await page.goto(`https://solscan.io/account/${address}`);
+    const responseRaw = await responsePromise;
+    const responseData = await responseRaw.json();
+
+    await browser.close();
+
+    res.status(200).json(responseData);
+
+    console.log("STOPPING SOLANA NAME SERVICE WORKER");
+
+  } catch (err) {
+    const out = {
+      error: "Something went wrong...",
+      message: err.message,
+    };
+
+    res.status(500).json(out);
+  }
+};
+
+export default handler;
