cafaf568e2411189e92ee5d3717db6c09f14e514
Andrei Albulescu <agalbulescu@gmail.com>
2023-05-28 00:25:02 +0300

    try for fix vercel path

--- escu-sns/pages/api/snsData/index.js
+++ escu-sns/pages/api/snsData/index.js
@@ -34,7 +34,7 @@ const handler = async (req, res) => {
 
     const browser = await chromium.launch({
         headless: true,
-        executablePath: '../../pw-browsers/chromium-1064/chrome-linux/chrome'
+        executablePath: '/home/.cache/ms-playwright/chromium-1064/chrome-linux/chrome'
     })
 
     const page = await browser.newPage(options)
