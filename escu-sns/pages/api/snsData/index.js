6f2bb58aa0033af84ddab38863c23db5ca170572
Andrei Albulescu <agalbulescu@gmail.com>
2023-05-28 13:45:22 +0300

    Update index.js

--- escu-sns/pages/api/snsData/index.js
+++ escu-sns/pages/api/snsData/index.js
@@ -36,7 +36,7 @@ const handler = async (req, res) => {
 
     const browser = await chromium.launch({
         headless: true,
-        executablePath: '../../../../pw-browsers/chromium-1064/chrome-linux/chrome'
+        executablePath: '../../../../.cache/ms-playwright/chromium-1064/chrome-linux/chrome'
     })
 
     const page = await browser.newPage(options)
